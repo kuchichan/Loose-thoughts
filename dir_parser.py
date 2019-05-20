ce276a2f18d9f3787623e587824a56bc75ee9706
kuchichan <pawel.kucharski@hotmail.com>
2019-05-20 21:24:32 +0200

    arg_parser added

--- dir_parser.py
+++ dir_parser.py
@@ -1,7 +1,9 @@
 import json
 import os
+import argparse
 
-def dir2json(base_path):
+def path_2_json(base_path):
+    """Path to json converter (non-recursive)."""
     initial = {
         "name": os.path.basename(base_path),
         "path": os.path.abspath(base_path),
@@ -17,10 +19,18 @@ def dir2json(base_path):
             last['type'] = "directory"
             last['children'] = []
             for elem in list_of_dir:
-                temp_dir = {'name': os.path.basename(elem),
-                            'path': os.path.join(last['path'], elem)}
+                temp_dir = {
+                    'name': os.path.basename(elem),
+                    'path': os.path.join(last['path'], elem)
+                }
                 last['children'].append(temp_dir)
                 stack.append(temp_dir)
 
     return json.dumps(initial, indent=4, separators=(',', ':'))
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory")
+    args = parser.parse_args()
+    print(path_2_json(args.directory))
